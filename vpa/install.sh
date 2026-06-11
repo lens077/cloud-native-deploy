bf1c0f83bf185f1c06f3403d9d2e1f2398d5babb
lens <rcc@vip.qq.com>
2026-06-12 04:32:26 +0800

    feat(vpa): 新增应用垂直扩缩容示例配置
    - 新增certManager、dragonfly、kafka、minio等应用的VPA配置示例
    - 添加jaeger、loki-gateway、postgres等VPA配置
    - 新增operator.yml和ui.yml配置
    - 添加helper.sh辅助脚本

--- vpa/install.sh
+++ vpa/install.sh
@@ -11,3 +11,16 @@ cd autoscaler/vertical-pod-autoscaler
 
 # 3. 验证安装 (查看 vpa-* Pod 是否为 Running 状态)
 kubectl get pods -n kube-system | grep vpa
+
+# 支持updateMode: "InPlace"模式需要
+kubectl patch deployment vpa-admission-controller -n kube-system --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--feature-gates=InPlace=true"}]'
+kubectl patch deployment vpa-updater -n kube-system --patch '
+spec:
+  template:
+    spec:
+      containers:
+      - name: updater
+        args:
+        - --v=4
+        - --feature-gates=InPlace=true
+'
